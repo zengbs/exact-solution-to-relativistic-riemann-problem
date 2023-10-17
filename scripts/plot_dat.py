141d76bfea2b8fee86e41b847a7190ec6dd82403
ChunYen-Chen <chunyenchen2019@gmail.com>
2023-10-17 15:06:34 +0800

    Minor

--- scripts/plot_dat.py
+++ scripts/plot_dat.py
@@ -2,10 +2,28 @@ import numpy as np
 import matplotlib.pylab as plt
 
 
-data = np.loadtxt("000001_TM.dat")
+N = 81
+for i in range(N):
+    data = np.loadtxt("../%06d_TM.dat"%i)
 
-fig, ax = plt.subplots(3, 1, figsize=(10, 5))
-ax[0].plot(data[:, 0], data[:, 1])
-ax[1].plot(data[:, 0], data[:, 2])
-ax[2].plot(data[:, 0], data[:, 3])
-plt.show()
+    fig, ax = plt.subplots(3, 1, figsize=(10, 9))
+    plt.subplots_adjust(hspace=0.0)
+
+    ax[0].plot(data[:, 0], data[:, 1])
+    ax[0].set(ylabel=r"$\rho$")
+    ax[0].set(xscale="log")
+    ax[0].set(yscale="log")
+
+    ax[1].plot(data[:, 0], data[:, 2] * 1.e-6)
+    ax[1].set(ylabel=r"$U/c [10^6]$")
+    ax[1].set(xscale="log")
+
+    ax[2].plot(data[:, 0], data[:, 3])
+    ax[2].set(ylabel=r"$P$")
+    ax[2].set(yscale="log")
+    ax[2].set(xscale="log")
+
+    plt.suptitle("ID = %06d"%i)
+    plt.savefig("%06d.png"%i)
+    plt.close()
+    #plt.show()
